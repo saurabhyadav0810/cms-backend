ac63005eb8a26fa4a53ecadcd199989fa9d4bd37
Saurabh Yadav <yadavsaurabhrajepur@gmail.com>
2026-02-12 10:52:07 +0530

    c2

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import path from "path";
+import fs from "fs";
+import webhookRoutes from "./webhook/webhooks.js";
+
 import authRoutes from "./routes/auth.route.js";
 import artifactRoutes from "./routes/artifacts.route.js"
 import likes from "./routes/likes.routes.js";
 import comment from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import { testing } from "./crons/testing.js";
+
 const app = express();
 
-/* Middlewares */
+
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
+
+
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(morgan("dev"));
-
-
-
 app.use(cookieParser());
-/* Test Route */
+
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
+
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
@@ -25,16 +36,13 @@ app.get("/", (req, res) => {
   });
 });
 
-app.use("/auth",authRoutes);
+
+testing();
+
+app.use("/webhooks", webhookRoutes);
+app.use("/auth", authRoutes);
 app.use("/artifacts", artifactRoutes);
 app.use("/likes", likes);
 app.use("/comments", comment);
+
 export default app;
-
-
-
-
-// app.use(cors({
-//   origin: ["https://cms-admin.vercel.app"],
-//   credentials: true
-// }));
